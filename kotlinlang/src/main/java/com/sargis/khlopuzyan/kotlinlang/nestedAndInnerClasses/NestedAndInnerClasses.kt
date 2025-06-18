a1af7730084615a3e6709aca837d4d04587ab0e5
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2025-06-18 15:33:19 +0400

    Nested and Inner classes

---
+++ kotlinlang/src/main/java/com/sargis/khlopuzyan/kotlinlang/nestedAndInnerClasses/NestedAndInnerClasses.kt
@@ -0,0 +1,27 @@
+package com.sargis.khlopuzyan.kotlinlang.nestedAndInnerClasses
+
+fun main() {
+    val nested = Outer.Nested()
+    val inner = Outer().Inner()
+
+    val innerClass = OuterInterface.InnerClass()
+    // Anonymous inner classes
+    val innerInterface = object : OuterInterface.InnerInterface {}
+}
+
+class Outer {
+    private val bar: Int = 1
+
+    class Nested {
+        fun foo() = 2
+    }
+
+    inner class Inner {
+        fun boo() = 4
+    }
+}
+
+interface OuterInterface {
+    class InnerClass
+    interface InnerInterface
+}
