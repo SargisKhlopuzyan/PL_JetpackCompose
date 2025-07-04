2ac54c59501b4eb4df7831e99aca48eeb9158d85
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2025-07-04 14:38:31 +0400

    Scope functions

---
+++ presentation/src/main/java/com/sargis/khlopuzyan/presentation/ui/scopeFunctions/ScopeFunctions.kt
@@ -0,0 +1,45 @@
+package com.sargis.khlopuzyan.presentation.ui.scopeFunctions
+
+fun main() {
+    val customClass = CustomClass()
+
+    // 1) run
+    val resultRun = customClass.run {
+        this
+        12
+    }
+
+    val resultRun2 = run {
+        12
+    }
+    // resultRun & resultRun2 are Int
+
+    // ****************************** //
+    // 2)
+    val resultWith = with(customClass) {
+        this
+        12
+    }
+    // resultWith is Int
+
+    // ****************************** //
+    // 3) let
+
+    val resultLet = customClass.let {
+        it
+        12
+    }
+
+    // resultLet is Int
+
+    // ****************************** //
+    // 4)
+
+    val resultAlso = customClass.also {
+        it
+        12
+    }
+    // resultAlso is CUSTOM CLASS
+}
+
+class CustomClass
